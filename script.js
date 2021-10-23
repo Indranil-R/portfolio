903ca259c0aed61b51df578b9bf0c19092738130
Indranil-R <87275536+Indranil-R@users.noreply.github.com>
2021-10-23 22:39:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggle() {
+  sub = document.getElementById("dropdown");
+  img = document.getElementById("menuimg");
+
+  if (sub.style.display != "none" && img.src != "Images/menu_white.svg") {
+    sub.style.display = "none";
+    img.src = "Images/menu_white.svg";
+  } else {
+    sub.style.display = "block";
+    img.src = "Images/close_white.svg";
+  }
+}
+
+function thanks() {
+    alert(
+      "Your Response has been Saved !!  Thank you very much 😊 for checking my website !!"
+    );
+  }
